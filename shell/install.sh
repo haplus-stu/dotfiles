37de73428ad5883bc17085e9e3b86f94e6af0641
Hasu <syu8.0324@outlook.jp>
2021-04-10 13:32:10 +0900

    edited

--- shell/install.sh
+++ shell/install.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/.config/nvim/
 ln -s $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -s $HOME/dotfiles/shell/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
-#vim 
+#vim
 ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/vim/plugin.vim $HOME/.plugin.vim
 ln -s $HOME/dotfiles/vim/editor.vim $HOME/.editor.vim
@@ -12,7 +12,7 @@ ln -s $HOME/dotfiles/vim/keymap.vim $HOME/.keymap.vim
 
 #terminal
 ln -s $HOME/dotfiles/shell/zshrc $HOME/.zshrc
-ln -s $HOME/dotfiles/shell/tmux_conf $HOME/.tmux_conf
+ln -s $HOME/dotfiles/shell/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/shell/ranger $HOME/.config/ranger
 
 #i3

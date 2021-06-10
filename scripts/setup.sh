a85460f821ffd559e8d7e07dfb4049f03c1b4aa7
Hasu <hshue83@gmail.com>
2021-06-10 09:29:40 +0900

    [feat] test ci

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
-set -eu pipefail
+set -eu
 
 ########################################################
 #           main
 ########################################################
+flag=${@}
+
 function main(){
   local current_dir
   current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
   source ${current_dir}/lib/utils.sh
 
-
-  if[[ "$1" == gui ]]; then
-    source ${current_dir}/lib/install-i3.sh
-    source ${current_dir}/lib/install-alacritty.sh
+  if [[ "${flag}" = "gui" ]]; then
+    echomsg "gui"
+    # source ${current_dir}/lib/install-i3.sh
+    # source ${current_dir}/lib/install-alacritty.sh
+  else
+    :
   fi
 
   # source ${current_dir}/lib/install-neovim-head.sh
